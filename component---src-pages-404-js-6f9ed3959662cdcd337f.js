c58fd503d39c105e06d3f9778993152c939fc90f
Richard <richard@newborntown.com>
2021-03-06 17:15:08 +0800

    Updates

---
+++ component---src-pages-404-js-6f9ed3959662cdcd337f.js
@@ -0,0 +1,2 @@
+(self.webpackChunklimit_oo=self.webpackChunklimit_oo||[]).push([[883],{9616:function(e,t,n){"use strict";n.r(t);var l=n(7294),u=n(5127),o=n(3751);t.default=function(){return l.createElement(u.Z,null,l.createElement(o.Z,{title:"404: Not found"}),l.createElement("h1",null,"404: Not Found"),l.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-6f9ed3959662cdcd337f.js.map
---
+++ component---src-pages-page-2-js-37e3387b87680b10f18c.js
@@ -0,0 +1,2 @@
+(self.webpackChunklimit_oo=self.webpackChunklimit_oo||[]).push([[617],{9863:function(e,t,l){"use strict";l.r(t);var n=l(7294),o=l(5444),a=l(5127),c=l(3751);t.default=function(){return n.createElement(a.Z,null,n.createElement(c.Z,{title:"Page two"}),n.createElement("h1",null,"Hi from the second page"),n.createElement("p",null,"Welcome to page 2"),n.createElement(o.Link,{to:"/"},"Go back to the homepage"))}}}]);
+//# sourceMappingURL=component---src-pages-page-2-js-37e3387b87680b10f18c.js.map
